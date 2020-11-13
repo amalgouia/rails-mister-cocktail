b1feec5edaf07e081e23abe071b26848f2e8e1d6
amalgouia <amal_gouia@hotmail.fr>
2020-11-13 11:26:06 +0100

    Mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,31 +1,64 @@
-class DosesController < ApplicationController
+# class DosesController < ApplicationController
 
+#   def new
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new
+#   end
+
+#   def create
+
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new(dose_params)
+#     @dose.cocktail = @cocktail
+
+#     if @dose.save
+#       redirect_to dose_path(@dose)
+#     else
+#       render :new
+#     end
+#   end
+
+
+#   def destroy
+#     @dose = Dose.find(params[:id])
+#     @dose.destroy
+#     redirect_to cocktails_path(@dose.cocktail)
+#   end
+
+#   private
+
+#   def dose_params
+#     params.require(:dose).permit(:description, :ingredient_id)
+#   end
+# end
+
+
+class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
-
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new(@dose)
-
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-
   def destroy
-
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
-  def description_params
-    params.require(:dose).permit(:description)
-  end
-   def ingredients_params
-    params.require(:dose).permit(:ingredients)
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
